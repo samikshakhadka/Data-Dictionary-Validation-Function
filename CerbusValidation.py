19483c44bce5ded33fbaa93443d2452d4ca1c392
Samiksha <swabhimankhadka2001@gmail.com>
2024-01-26 14:19:38 +0545

    feat(allow_unknown): ignore additional fields while data validating

--- CerbusValidation.py
+++ CerbusValidation.py
@@ -15,6 +15,7 @@ user_data_schema = {
     'age': {'type': 'integer', 'min': 18, 'max': 99},
     'time': {'validator': date_time_validate, 'nullable': True},
     'is_student': {'type': 'boolean'},
+
 }
 
 
@@ -22,16 +23,17 @@ user_data_input = {
     'name': input("Enter your name: "),
     'age': int(input("Enter your age: ")),
     'time': input("Enter the time (YYYY-MM-DD HH:MM:SS) or press Enter for None: "),
-    'is_student': input("Are you a student? (True/False): ").lower() == 'true'
+    'is_student': input("Are you a student? (True/False): ").lower() == 'true',
+    'extra_field': input("Enter an extra field (will be ignored by validation): ")
+
 }
 
 
-validator = Validator(user_data_schema)
+# allow_unknown=True : any additional field will be ignored
+validator = Validator(user_data_schema, allow_unknown=True)
 if validator.validate(user_data_input):
     print("Data is valid.")
 
-    if 'time' in user_data_input and user_data_input['time']:
-        user_data_input['time'] = datetime.strptime(user_data_input['time'], "%Y-%m-%d %H:%M:%S")
-    print("Processed user data:", user_data_input)
+
 else:
     print("Data validation failed. Errors:", validator.errors)
